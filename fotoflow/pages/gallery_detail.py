e47c0390ddb48f0e716b6ae199c4aa3c5ad99ea9
Manuel Andreu <Manuel.andreu@gmail.com>
2025-01-18 17:06:22 +0100

    Leo el parametro id

--- fotoflow/pages/gallery_detail.py
+++ fotoflow/pages/gallery_detail.py
@@ -3,8 +3,18 @@ import reflex as rx
 from ..components.navbar import navbar
 from ..components.auth_wrapper import require_auth
 
+
+class GalleryState(rx.State):
+    # current_gallery: Galery | None = None
+
+    @rx.var
+    def gallery_id(self) -> int:
+        """Obtiene el ID de la galería de la URL"""
+        return int(self.router.page.params.get("id", 0))
+
+
 @require_auth
-def gallery_detail():
+def gallery_detail() -> rx.Component:  # Añadimos el parámetro id
     """Página de detalle de galería"""
     return rx.vstack(
         navbar(),
@@ -12,7 +22,7 @@ def gallery_detail():
             # Cabecera
             rx.hstack(
                 rx.heading(
-                    f"Galería: XXX",
+                    f"Galería:  {GalleryState.gallery_id}",  # Usamos el id recibido
                     size="6",
                 ),
                 rx.spacer(),
@@ -24,7 +34,6 @@ def gallery_detail():
                 width="100%",
                 padding="4",
             ),
-            
             # Detalles de la galería
             rx.box(
                 rx.vstack(
@@ -52,7 +61,6 @@ def gallery_detail():
                 box_shadow="sm",
                 padding="4",
             ),
-            
             width="100%",
             max_width="1200px",
             margin="0 auto",
